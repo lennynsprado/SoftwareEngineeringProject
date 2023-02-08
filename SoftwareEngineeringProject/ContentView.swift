5c744e8a4cb38ef3a4e7b8716183f92303bffe97
lennynsprado <lennyn_michaelprado@hotmail.com>
2023-02-07 20:34:32 -0500

    Created Login Page

--- SoftwareEngineeringProject/ContentView.swift
+++ SoftwareEngineeringProject/ContentView.swift
@@ -8,19 +8,80 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    //Variables
+    @State private var username = ""
+    @State private var password = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        ZStack{
+            Color.black
+            
+            RoundedRectangle(cornerRadius: 30, style: .continuous)
+                .foregroundStyle(.linearGradient(colors: [.pink, .red], startPoint: .topLeading, endPoint: .bottomTrailing))
+                .frame(width: 1000, height: 400)
+                .rotationEffect(.degrees(135))
+                .offset(y: -350)
+            
+            VStack(spacing: 20){
+                Text("MScoots")
+                    .foregroundColor(.white)
+                    .font(.system(size: 40, weight: .bold, design: .rounded))
+                    .offset(x: -90, y: -250)
+                TextField("Username", text: $username)
+                    .foregroundColor(.white)
+                    .textFieldStyle(.plain)
+                    .offset(y: -170)
+                    .placeholder(when: username.isEmpty){
+                        Text("Username")
+                            .foregroundColor(.white)
+                            .bold()
+                            .offset(y: -170)
+                    }
+                
+                Rectangle()
+                    .frame(width: 350, height: 3)
+                    .foregroundColor(.white)
+                    .offset(y: -170)
+                
+                SecureField("Password", text: $password)
+                    .foregroundColor(.white)
+                    .textFieldStyle(.plain)
+                    .offset(y: -160)
+                    .placeholder(when: password.isEmpty){
+                        Text("Password")
+                            .foregroundColor(.white)
+                            .bold()
+                            .offset(y: -160)
+                    }
+                
+                Rectangle()
+                    .frame(width: 350, height: 3)
+                    .foregroundColor(.white)
+                    .offset(y: -160)
+            }
+            .frame(width: 350)
         }
-        .padding()
+        .ignoresSafeArea()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+extension View{
+    func placeholder<Content: View>(
+        when shouldShow: Bool,
+        alignment: Alignment = .leading,
+        @ViewBuilder placeholder: () -> Content) -> some View {
+            
+            ZStack(alignment: alignment){
+                placeholder().opacity(shouldShow ? 1 : 0)
+                self
+            }
     }
 }
