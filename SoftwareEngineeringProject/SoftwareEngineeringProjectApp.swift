487d20f9e92234e4322111b6b9a2722737a8137e
lennynsprado <lennyn_michaelprado@hotmail.com>
2023-02-07 19:52:17 -0500

    Initial Commit

---
+++ SoftwareEngineeringProject/SoftwareEngineeringProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoftwareEngineeringProjectApp.swift
+//  SoftwareEngineeringProject
+//
+//  Created by Lennyn Stephano Prado on 2/7/23.
+//
+
+import SwiftUI
+
+@main
+struct SoftwareEngineeringProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
